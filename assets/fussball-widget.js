9d01db102ffe9fe52917683b59b42eb151bb7bd4
kozi <martin@kozianka.de>
2013-08-16 10:40:55 +0200

    Changes for new widget version

--- assets/fussball-widget.js
+++ assets/fussball-widget.js
@@ -4,62 +4,71 @@
  * Copyright (c) 2011-2013 Martin Kozianka
  *
  */
+if (!String.prototype.fulltrim) {
+    String.prototype.fulltrim=function(){return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');};
+}
 
 var FussballWidget = function() {
-	var id = null;
-	var div_id = null;
-	var team = null;
-	var wettbewerb = null;
-	
-return {
+    var id = null;
+    var div_id = null;
+    var team = null;
+    var wettbewerb = null;
 
-	ergebnisse: function() {
-		this.wettbewerb.zeigeBegegnungen(this.div_id);
-		return false;
-	},
-	tabelle: function() {
-		this.wettbewerb.zeigeTabelle(this.div_id);
-		return false;
+    return {
 
-	},
-	init: function(id, wettbewerbs_id, mandant, team) {
-		// id setzen
-		this.id = id;
-		this.div_id = 'id' + this.id;
-		
-		this.wettbewerb = new fussballdeAPI();
-
-        this.wettbewerb.setzeWettbewerb(wettbewerbs_id);
-        this.wettbewerb.setzeMandant(mandant);
-		
-		if (team.length > 0) {
-			this.team = team;
-			this.highlight_team();
-		}
-	},
-
-	highlight_team: function() {
-		window.setTimeout(this.id + '.highlight_team()', 2000);
-
-		divEl = document.getElementById('fussballdeAPI');
-		if (divEl == null || document.getElementById('highlighted_row') != null) { 
+        ergebnisse: function() {
+            this.wettbewerb.zeigeBegegnungen(this.div_id);
             return false;
-		}
+        },
+        tabelle: function() {
+            this.wettbewerb.zeigeTabelle(this.div_id);
+            return false;
+        },
+        init: function(id, wettbewerbs_id, mandant, team) {
+            // id setzen
+            this.id = id;
+            this.div_id = 'id' + this.id;
 
-		a_nodes = divEl.getElementsByTagName('a');
+            this.wettbewerb = new fussballdeAPI();
 
-		for (i = 0;i < a_nodes.length; i++) {
-			node = a_nodes[i].firstChild;
-			if (node.nodeType == 3 && node.nodeValue.indexOf(this.team) != -1) {
-				row = node.parentNode.parentNode.parentNode;
-				row.id = "fussball_widget_highlighted_row";
-				
-				return true;
-			}
-		}
-		
-	}
+            this.wettbewerb.setzeWettbewerb(wettbewerbs_id);
+            this.wettbewerb.setzeMandant(mandant);
+
+            if (team.length > 0) {
+                this.team = team.toLowerCase().fulltrim();
+                this.highlight_team();
+            }
+        },
+
+        highlight_team: function() {
+            window.setTimeout(this.id + '.highlight_team()', 2000);
+
+            if (document.getElementById('highlighted_row') != null) {
+                return false;
+            }
+
+            divEl = document.getElementById('fussballdeAPI');
+            if (divEl == null) { return false; }
+
+            table = document.getElementById(this.div_id).getElementsByClassName('egmSnippetContent')[0];
+            if (table == null) { return false; }
+
+            a_nodes = table.getElementsByTagName('a');
+
+            for (i = 0;i < a_nodes.length; i++) {
+                node = a_nodes[i].firstChild;
+                if (node.nodeType == 3) {
+                    value = node.nodeValue.toLowerCase().fulltrim();
+                    if (value == this.team) {
+                        row = node.parentNode.parentNode.parentNode;
+                        row.id = "fussball_widget_highlighted_row";
+                    }
+                }
+            }
+
+        }
+
+    };
 
 };
 
-};
